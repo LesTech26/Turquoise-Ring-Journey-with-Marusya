8fa5d601eef80c41b2d231dd0109f35e3c015059
wersiee <120658212+sadsoonya@users.noreply.github.com>
2026-06-23 23:28:25 +0300

    создан модуль biruse_kolco (Разработчик 1)
    создан модуль biruse_kolco (Разработчик 1) - SplashScreen, главный экран, настройки, достижения, Room БД, Navigation

---
+++ biruse_kolco/src/main/java/com/example/biruse_kolco/utils/Constants.java
@@ -0,0 +1,48 @@
+package com.example.biruse_kolco.utils;
+
+public class Constants {
+
+    // База данных
+    public static final String DATABASE_NAME = "biruse_kolco_db";
+    public static final int DATABASE_VERSION = 1;
+
+    // Пользователь
+    public static final int DEFAULT_USER_ID = 1;
+    public static final String DEFAULT_USER_NAME = "Путешественник";
+
+    // Игры
+    public static final String GAME_VIKTORINA = "viktorina";
+    public static final String GAME_KROSSVORD = "krossvord";
+    public static final String GAME_RASKASKA = "raskaska";
+    public static final String GAME_UGADAY_RAYON = "ugaday_rayon";
+    public static final String GAME_SOBERI_KOSTUM = "soberi_kostum";
+    public static final String GAME_PAZL = "pazl";
+    public static final String GAME_MEMORI = "memori";
+    public static final String GAME_NAYDI_OTLICHIE = "naydi_otlichie";
+    public static final String GAME_PRAVDA_NEPRAVDA = "pravda_nepravda";
+
+    // Очки
+    public static final int POINTS_PER_DISTRICT = 10;
+    public static final int POINTS_PER_QUESTION = 5;
+    public static final int POINTS_PER_GAME_WIN = 15;
+    public static final int POINTS_PER_ACHIEVEMENT = 25;
+
+    // Достижения
+    public static final int ACHIEVEMENT_FIRST_STEP = 1;
+    public static final int ACHIEVEMENT_YOUNG_EXPLORER = 5;
+    public static final int ACHIEVEMENT_KNOWLEDGEABLE = 10;
+    public static final int ACHIEVEMENT_MASTER = 24;
+    public static final int ACHIEVEMENT_HISTORY_LOVER = 3;
+    public static final int ACHIEVEMENT_CULTURE_LOVER = 5;
+    public static final int ACHIEVEMENT_TRAVELER = 12;
+
+    // Категории достижений
+    public static final String CATEGORY_DISTRICTS = "districts";
+    public static final String CATEGORY_GAMES = "games";
+    public static final String CATEGORY_COSTUMES = "costumes";
+
+    // TODO: Для Разработчика 2 - добавить константы для районов
+    // TODO: Для Разработчика 3 - добавить константы для маскота
+    // TODO: Для Разработчика 4 - добавить константы для игр
+    // TODO: Для Разработчика 5 - добавить константы для админ-панели
+}
